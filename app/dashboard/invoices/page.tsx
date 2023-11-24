f2f92c429af8f2945de95b20c79462e5b37eb376
Husein Naimov <huseinnaimov@bk.ru>
2023-11-24 12:53:58 +0500

    added README.md

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -17,7 +17,7 @@ export default async function Page({
   searchParams?: {
     query?: string;
     page?: string;
-  };
+  }; 
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
